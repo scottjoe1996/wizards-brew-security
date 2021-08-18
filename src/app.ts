e022f5494f468c904d4d1a9f755b92812baec846
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-18 18:10:45 +0800

    Delete src directory

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const app = express();
-
-app.get('/', (_request: Request, response: Response) => {
-  response.send('Hello world');
-});
-
-app.listen(5000, () => console.log('Server running'));
